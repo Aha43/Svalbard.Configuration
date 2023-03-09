07fa2af228025867fa6fa10fb260924aa5321013
Arne Halvorsen <arha@dips.no>
2023-03-09 22:00:05 +0100

    wip: methods to bind config section to type having the name of the section

---
+++ src/Svalbard.Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Svalbard.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static T? GetAs<T>(this IConfiguration configuration, string? name = default) where T : class => 
+            configuration.GetSection(name ?? nameof(T))?.Get<T>();
+
+        public static T GetRequiredAs<T>(this IConfiguration configuration, string? name = default) where T : class
+        {
+            var retVal = configuration.GetAs<T>(name);
+            if (retVal == null) throw new ArgumentException($"Configuration section named '{name ?? nameof(T)}' not found");
+            return retVal;
+        }
+
+    }
+
+}
